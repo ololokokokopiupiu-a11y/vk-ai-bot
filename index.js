15ef586847a9b760e8e0a787a3a6c85d9b72583a
ololokokokopiupiu-a11y <ololokokokopiupiu@gmail.com>
2025-12-25 17:57:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ app.post("/", async (req, res) => {
   if (body.type === "message_new") {
     const userId = body.object.message.from_id;
 
-    await fetch("https://api.vk.com/method/messages.send", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        peer_id: userId,
-        message: "Бот жив и отвечает ✅",
-        random_id: Date.now(),
-        access_token: VK_TOKEN,
-        v: "5.131"
+   const vkResponse = await fetch("https://api.vk.com/method/messages.send", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    peer_id: userId,
+    message: "Бот жив и отвечает ✅",
+    random_id: Date.now(),
+    access_token: VK_TOKEN,
+    v: "5.131"
       })
     });
   }
